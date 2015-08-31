65aa23d793d50849eb0383b2972d3709a6ce1e10
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-08-31 11:10:46 -0400

    Apache License 2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description='Python client for parsing SCOTUS data from multiple sources.',
     long_description=read('README.rst'),
     packages=['clerk'],
-    license="MIT",
+    license="Apache License 2.0",
     keywords='SCOTUS data parsing scraping legal law court',
     install_requires=['beautifulsoup4==4.4.0','html5lib==0.999999','lxml==3.4.4','requests==2.7.0','six==1.9.0','wheel==0.24.0'],
     classifiers=['Development Status :: 4 - Beta',
