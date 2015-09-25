b3387f2cff371ec2ad4e45aa1c08e087d7088b7f
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-09-25 16:44:39 -0400

    Proper syntax.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=['clerk'],
     license="Apache License 2.0",
     keywords='SCOTUS data parsing scraping legal law court',
-    install_requires=['beautifulsoup4==4.4.0','html5lib==0.999999','lxml==3.4.4','requests==2.7.0','six==1.9.0','wheel==0.24.0','nameparser-0.3.10'],
+    install_requires=['beautifulsoup4==4.4.0','html5lib==0.999999','lxml==3.4.4','requests==2.7.0','six==1.9.0','wheel==0.24.0','nameparser==0.3.10'],
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
                  'Programming Language :: Python',
