8290eab1eedd4d6b7f8d2cd558ce7d6161cf1582
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-07-05 16:28:14 -0400

    Apparently, it does not work.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(filename):
 
 setup(
     name='nyt-clerk',
-    version='0.1.9',
+    version='0.1.10',
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/nyt-clerk',
@@ -26,7 +26,7 @@ setup(
     },
     license="Apache License 2.0",
     keywords='SCOTUS data parsing scraping legal law court',
-    install_requires=['beautifulsoup4','html5lib','lxml','requests','nameparser','csvkit', 'pymongo', 'clint'],
+    install_requires=['beautifulsoup4','html5lib','lxml','requests','nameparser','csvkit', 'pymongo', 'clint', 'cement'],
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
                  'Programming Language :: Python',
